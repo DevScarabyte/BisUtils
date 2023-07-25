6dc818657137f26045baa6a1ca87d294ccf2f1f6
Scarabyte <heihachi@prontonmail.com>
2023-07-25 04:18:39 -0800

    Rewrite LZSS applied

--- BisUtils.Core/Compression/Externals/BisCompatibleLZSS.cs
+++ BisUtils.Core/Compression/Externals/BisCompatibleLZSS.cs
@@ -1,163 +1,142 @@
 ﻿namespace BisUtils.Core.Compression.Externals;
 
 /// <summary>
-/// LZSS compression variant compatible with BI games 
+///     Bis Lzss
 /// </summary>
-/// <remarks>
-/// Based on LZSS.c by Haruhiko Okumura
-/// https://dev.gameres.com/Program/Other/LZSS.C
-/// and its adaptation from bis-file-formats
-/// https://github.com/jetelain/bis-file-formats/blob/master/BIS.Core/Compression/LzssCompression.cs
-/// </remarks>
-internal sealed class BisCompatibleLZSS {
-    private const int N = 4096;
-    private const int F = 18;
-    private const byte FILL = 0x20;
-    private const int THRESHOLD = 2;
+/// Author: https://github.com/rvost Thank You So Much
+public sealed class BisCompatibleLZSS
+{
+    public static BisCompatibleLZSS Compressor { get; } = new BisCompatibleLZSS();
+    private const int N = 4096, F = 18, Threshold = 2;
+    private const byte Fill = 0x20;
 
-    // index for root of binary search trees
-    private readonly int NIL;
+    /// <summary>
+    ///     Index for root of binary search trees
+    /// </summary>
+    private readonly int nil = N;
 
-    // ring buffer of size N, with extra F-1 bytes to facilitate string comparison.
-    private readonly byte[] textBuf;
+    /// <summary>
+    ///     These constitute binary search trees.
+    /// </summary>
+    private readonly int[] previousChildren = new int[N + 1], nextChildren = new int[N + 257], parents = new int[N + 1];
 
-    // These constitute binary search trees.
-    // left children right children &amp; parents 
-    private readonly int[] lson, rson, dad;
+    /// <summary>
+    ///     The ring buffer of size N with extra F-1 bytes to facilitate string comparison.
+    /// </summary>
+    private readonly byte[] textBuffer = new byte[N + F - 1];
 
-    private int matchPosition, matchLength; // of longest match.  These are set by the InsertNode() procedure.
+    /// <summary>
+    ///     The length and position of the longest match found.
+    ///     These are set by the InsertNode() procedure.
+    /// </summary>
+    private int matchPosition, matchLength;
 
-    public BisCompatibleLZSS() {
-        NIL = N;
-        textBuf = new byte[N + F - 1];
-        lson = new int[N + 1];
-        rson = new int[N + 257];
-        dad = new int[N + 1];
-    }
-
-    // Initialize trees.
-    private void InitTree() {
-        int i;
-
-        /* For i = 0 to N - 1, rson[i] and lson[i] will be the right and
-           left children of node i.  These nodes need not be initialized.
-           Also, dad[i] is the parent of node i.  These are initialized to
-           NIL (= N), which stands for 'not used.'
-           For i = 0 to 255, rson[N + i + 1] is the root of the tree
-           for strings that begin with character i.  These are initialized
-           to NIL.  Note there are 256 trees. */
-
-        for (i = N + 1; i <= N + 256; i++) {
-            rson[i] = NIL;
-        }
-
-        for (i = 0; i < N; i++) {
-            dad[i] = NIL;
-        }
-    }
-
-    /* Inserts string of length F, text_buf[r..r+F-1], into one of the
-       trees (text_buf[r]'th tree) and returns the longest-match position
-       and length via the global variables match_position and match_length.
-       If match_length = F, then removes the old node in favor of the new
-       one, because the old one will be deleted sooner.
-       Note r plays double role, as tree node and position in buffer. 
-    */
-    private void InsertNode(int r)
+    public void Decode(BinaryReader reader, BinaryWriter output, uint length)
     {
+        var text_buf = new byte[N + F - 1];
+        var outputBytes = new byte[length];
+
+        if (length <= 0) {
+            output.Write(outputBytes);
+            return;
+        }
+
         int i;
-        var cmp = 1;
-        matchLength = 0;
+        var flags = 0;
+        uint iDst = 0, bytesLeft = length;
+        Array.Fill(text_buf, Fill);
+        for (i = 0; i < N - F; i++)
+        {
+            text_buf[i] = 0x20;
+        }
 
-        var p = N + 1 + textBuf[r];
+        var r = N - F;
 
-        rson[r] = lson[r] = NIL;
-        for (; ; ) {
-            if (cmp >= 0) {
-                if (rson[p] != NIL) {
-                    p = rson[p];
-                }
-                else {
-                    rson[p] = r;
-                    dad[r] = p;
-                    return;
-                }
-            }
-            else
-            {
-                if (lson[p] != NIL) {
-                    p = lson[p];
-                }
-                else {
-                    lson[p] = r;
-                    dad[r] = p;
-                    return;
-                }
-            }
-            for (i = 1; i < F; i++) {
-                if ((cmp = textBuf[r + i] - textBuf[p + i]) != 0) break;
+        while (bytesLeft > 0) {
+            int c;
+            if (((flags >>= 1) & 256) == 0) {
+                c = reader.ReadByte();
+                flags = c | 0xff00;
             }
 
-            if (i > matchLength) {
-                matchPosition = p;
-                if ((matchLength = i) >= F) break;
+            if ((flags & 1) != 0) {
+                c = reader.ReadByte();
+
+                // save byte
+                outputBytes[iDst++] = (byte) c;
+                bytesLeft--;
+                // continue decompression
+                text_buf[r] = (byte) c;
+                r++;
+                r &= N - 1;
+            }
+            else {
+                i = reader.ReadByte();
+                int j = reader.ReadByte();
+                i |= (j & 0xf0) << 4;
+                j &= 0x0f;
+                j += Threshold;
+
+                int ii = r - i,
+                    jj = j + ii;
+
+                if (j + 1 > bytesLeft) {
+                    throw new ArgumentException("LZSS overflow");
+                }
+
+                for (; ii <= jj; ii++) {
+                    c = text_buf[ii & (N - 1)];
+
+                    // save byte
+                    outputBytes[iDst++] = (byte) c;
+                    bytesLeft--;
+                    // continue decompression
+                    text_buf[r] = (byte) c;
+                    r++;
+                    r &= N - 1;
+                }
             }
         }
-        dad[r] = dad[p];
-        lson[r] = lson[p];
-        rson[r] = rson[p];
-        dad[lson[p]] = r;
-        dad[rson[p]] = r;
-        if (rson[dad[p]] == p) {
-            rson[dad[p]] = r;
-        }
-        else {
-            lson[dad[p]] = r;
-        }
-        
-        dad[p] = NIL;  // remove p
+        reader.ReadInt32();
+        output.Write(outputBytes);
     }
 
-    // Deletes node p from tree
-    private void DeleteNode(int p) {
-        int q;
-
-        if (dad[p] == NIL) return;  /* not in tree */
-        if (rson[p] == NIL) {
-            q = lson[p];
-        }
-        else if (lson[p] == NIL) {
-            q = rson[p];
-        }
-        else {
-            q = lson[p];
-            if (rson[q] != NIL) {
-                do {
-                    q = rson[q];
-                } while (rson[q] != NIL);
-                rson[dad[q]] = lson[q];
-                dad[lson[q]] = dad[q];
-                lson[q] = lson[p];
-                dad[lson[p]] = q;
-            }
-            rson[q] = rson[p];
-            dad[rson[p]] = q;
-        }
-        dad[q] = dad[p];
-        if (rson[dad[p]] == p) {
-            rson[dad[p]] = q;
-        }
-        else {
-            lson[dad[p]] = q;
-        }
-        dad[p] = NIL;
-    }
-
-    public int Encode(byte[] input, BinaryWriter output)
+    public Stream Encode(Stream inputStream, out uint outputSize)
     {
-        int i, len, last_match_length;
-        byte mask, c;
-        var codesize = 0;
+        if (!inputStream.CanRead)
+        {
+            throw new IOException("Cannot read from the provided stream.");
+        }
+
+        using var memoryStream = new MemoryStream();
+        inputStream.CopyTo(memoryStream);
+
+        var encodedStream = new MemoryStream();
+        using var binaryWriter = new BinaryWriter(encodedStream);
+
+        outputSize = Encode(memoryStream.ToArray(), binaryWriter);
+
+        encodedStream.Seek(0, SeekOrigin.Begin);
+        return encodedStream;
+    }
+
+    public uint Encode(Stream inputStream, BinaryWriter output)
+    {
+        if (!inputStream.CanRead)
+        {
+            throw new IOException("Cannot read from the provided stream.");
+        }
+
+        using var memoryStream = new MemoryStream();
+        inputStream.CopyTo(memoryStream);
+        return Encode(memoryStream.ToArray(), output);
+    }
+
+    public uint Encode(byte[] input, BinaryWriter output)
+    {
+        int i, len;
+        byte mask;
+        uint codeSize = 0;
 
         var codeBufIdx = mask = 1;
         var s = 0;
@@ -168,89 +147,101 @@ internal sealed class BisCompatibleLZSS {
 
         InitTree();
 
-        /* code_buf[1..16] saves eight units of code, 
-         * and code_buf[0] works as eight flags, 
-         * "1" representing that the unit is an unencoded letter (1 byte), 
-         * "0" a position-and-length pair (2 bytes).  
-         * Thus, eight units require at most 16 bytes of code. 
+        /* code_buf[1..16] saves eight units of code,
+         * and code_buf[0] works as eight flags,
+         * "1" representing that the unit is an unencoded letter (1 byte),
+         * "0" a position-and-length pair (2 bytes).
+         * Thus, eight units require at most 16 bytes of code.
          */
         codeBuf[0] = 0;
-        
+
         // Clear the buffer with any character that will appear often.
-        for (i = s; i < r; i++) {
-            textBuf[i] = FILL;
+        for (i = s; i < r; i++)
+        {
+            textBuffer[i] = Fill;
         }
 
-        for (len = 0; len < F && inputIdx < stopPos; len++) {
-            textBuf[r + len] = input[inputIdx++];  // Read F bytes into the last F bytes of the buffer
+        for (len = 0; len < F && inputIdx < stopPos; len++)
+        {
+            textBuffer[r + len] = input[inputIdx++]; // Read F bytes into the last F bytes of the buffer
         }
 
-        if (len == 0) {
-            return 0;  /* text of size zero */
+        if (len == 0)
+        {
+            return 0; /* text of size zero */
         }
+
         for (i = 1; i <= F; i++)
         {
-            /* 
+            /*
              * Insert the F strings, each of which begins with one or more 'space' characters.
              * Note the order in which these strings are inserted.
-             * This way,  degenerate trees will be less likely to occur. 
+             * This way,  degenerate trees will be less likely to occur.
              */
             InsertNode(r - i);
         }
-        InsertNode(r);  /* Finally, insert the whole string just read.  The
+
+        InsertNode(r); /* Finally, insert the whole string just read.  The
                                   global variables match_length and match_position are set. */
         do
         {
             if (matchLength > len)
             {
-                matchLength = len;  /* match_length
+                matchLength = len; /* match_length
                                                                 may be spuriously long near the end of text. */
             }
-            if (matchLength <= THRESHOLD)
+
+            if (matchLength <= Threshold)
             {
-                matchLength = 1;  /* Not long enough match.  Send one byte. */
-                codeBuf[0] |= mask;  /* 'send one byte' flag */
-                codeBuf[codeBufIdx++] = textBuf[r];  /* Send uncoded. */
+                matchLength = 1; /* Not long enough match.  Send one byte. */
+                codeBuf[0] |= mask; /* 'send one byte' flag */
+                codeBuf[codeBufIdx++] = textBuffer[r]; /* Send decoded. */
             }
             else
             {
                 /* Send position and length pair. Note match_length > THRESHOLD. */
-                // The difference between common compression implementations and the BI variant is the match position format.
-                var encoded_position = r - matchPosition & N - 1;
+                var encoded_position = (r - matchPosition) & (N - 1);
                 codeBuf[codeBufIdx++] = (byte)encoded_position;
-                codeBuf[codeBufIdx++] = (byte)(encoded_position >> 4 & 0xf0 | matchLength - (THRESHOLD + 1));
+                codeBuf[codeBufIdx++] = (byte)(((encoded_position >> 4) & 0xf0) | (matchLength - (Threshold + 1)));
             }
+
             if ((mask <<= 1) == 0) /* Shift mask left one bit. */
             {
                 output.Write(codeBuf[..codeBufIdx]); /* Send at most 8 units of code together */
-                codesize += codeBufIdx;
+                codeSize += codeBufIdx;
                 codeBuf[0] = 0;
                 codeBufIdx = mask = 1;
             }
-            last_match_length = matchLength;
-            for (i = 0; i < last_match_length && inputIdx < stopPos; i++)
+
+            var lastMatchLength = matchLength;
+            for (i = 0; i < lastMatchLength && inputIdx < stopPos; i++)
             {
                 DeleteNode(s); // Delete old strings and
-                c = input[inputIdx++];
-                textBuf[s] = c; // read new bytes
+                var c = input[inputIdx++];
+                textBuffer[s] = c; // read new bytes
                 if (s < F - 1)
                 {
-                    textBuf[s + N] = c; // If the position is near the end of buffer, extend the buffer to make  string comparison easier.
+                    textBuffer[s + N] =
+                        c; // If the position is near the end of buffer, extend the buffer to make  string comparison easier.
                 }
-                s = s + 1 & N - 1;
-                r = r + 1 & N - 1;
+
+                s = (s + 1) & (N - 1);
+                r = (r + 1) & (N - 1);
                 /* Since this is a ring buffer, increment the position modulo N. */
-                InsertNode(r);    /* Register the string in text_buf[r..r+F-1] */
+                InsertNode(r); /* Register the string in text_buf[r..r+F-1] */
             }
 
-            while (i++ < last_match_length)
+            while (i++ < lastMatchLength)
             {
                 /* After the end of text, */
                 DeleteNode(s); /* no need to read, but */
-                s = s + 1 & N - 1;
-                r = r + 1 & N - 1;
+                s = (s + 1) & (N - 1);
+                r = (r + 1) & (N - 1);
                 --len;
-                if (len != 0) InsertNode(r);        /* buffer may not be empty. */
+                if (len != 0)
+                {
+                    InsertNode(r); /* buffer may not be empty. */
+                }
             }
         } while (len > 0); /* until length of string to be processed is zero */
 
@@ -258,9 +249,174 @@ internal sealed class BisCompatibleLZSS {
         {
             /* Send remaining code. */
             output.Write(codeBuf[..codeBufIdx]);
-            codesize += codeBufIdx;
+            codeSize += codeBufIdx;
         }
+
         output.Flush();
-        return codesize;
+        return codeSize;
+    }
+
+    /// <summary>
+    ///     Initializes the binary search trees used by the compression algorithm.
+    /// </summary>
+    private void InitTree()
+    {
+        int i;
+        /*
+         For i = 0 to N - 1, NextChildren[i] and PreviousChildren[i] will be the right and
+         left children of node i.  These nodes need not be initialized.
+         Also, Parents[i] is the parent of node i.  These are initialized to
+         Nil (= N), which stands for 'not used.'
+         For i = 0 to 255, NextChildren[N + i + 1] is the root of the tree
+         for strings that begin with character i.  These are initialized
+         to Nil.  Note there are 256 trees.
+       */
+
+        for (i = N + 1; i <= N + 256; i++)
+        {
+            nextChildren[i] = nil;
+        }
+
+        for (i = 0; i < N; i++)
+        {
+            parents[i] = nil;
+        }
+    }
+
+    /// <summary>
+    ///     Inserts string of length F, TextBuffer[node..node+F-1], into one of the
+    ///     trees (TextBuffer[node]'th tree) and returns the longest-match position
+    ///     and length via the global variables MatchPosition and MatchLength.
+    ///     If MatchLength = F, then removes the old node in favor of the new
+    ///     one, because the old one will be deleted sooner.
+    /// </summary>
+    /// <param name="node">plays double role, as tree node and position in buffer.</param>
+    private void InsertNode(int node)
+    {
+        var cmp = 1;
+        matchLength = 0;
+
+        var p = N + 1 + textBuffer[node];
+
+        nextChildren[node] = previousChildren[node] = nil;
+        for (;;)
+        {
+            if (cmp >= 0)
+            {
+                if (nextChildren[p] != nil)
+                {
+                    p = nextChildren[p];
+                }
+                else
+                {
+                    nextChildren[p] = node;
+                    parents[node] = p;
+                    return;
+                }
+            }
+            else
+            {
+                if (previousChildren[p] != nil)
+                {
+                    p = previousChildren[p];
+                }
+                else
+                {
+                    previousChildren[p] = node;
+                    parents[node] = p;
+                    return;
+                }
+            }
+
+            int i;
+            for (i = 1; i < F; i++)
+            {
+                if ((cmp = textBuffer[node + i] - textBuffer[p + i]) != 0)
+                {
+                    break;
+                }
+            }
+
+            if (i <= matchLength)
+            {
+                continue;
+            }
+
+            matchPosition = p;
+            if ((matchLength = i) >= F)
+            {
+                break;
+            }
+        }
+
+        parents[node] = parents[p];
+        previousChildren[node] = previousChildren[p];
+        nextChildren[node] = nextChildren[p];
+        parents[previousChildren[p]] = node;
+        parents[nextChildren[p]] = node;
+        if (nextChildren[parents[p]] == p)
+        {
+            nextChildren[parents[p]] = node;
+        }
+        else
+        {
+            previousChildren[parents[p]] = node;
+        }
+
+        parents[p] = nil;
+    }
+
+    /// <summary>
+    ///     Deletes node n from tree
+    /// </summary>
+    /// <param name="n">Node to remove from tree</param>
+    private void DeleteNode(int n)
+    {
+        int q;
+
+        if (parents[n] == nil)
+        {
+            return; /* not in tree */
+        }
+
+        if (nextChildren[n] == nil)
+        {
+            q = previousChildren[n];
+        }
+        else if (previousChildren[n] == nil)
+        {
+            q = nextChildren[n];
+        }
+        else
+        {
+            q = previousChildren[n];
+            if (nextChildren[q] != nil)
+            {
+                do
+                {
+                    q = nextChildren[q];
+                } while (nextChildren[q] != nil);
+
+                nextChildren[parents[q]] = previousChildren[q];
+                parents[previousChildren[q]] = parents[q];
+                previousChildren[q] = previousChildren[n];
+                parents[previousChildren[n]] = q;
+            }
+
+            nextChildren[q] = nextChildren[n];
+            parents[nextChildren[n]] = q;
+        }
+
+        parents[q] = parents[n];
+        if (nextChildren[parents[n]] == n)
+        {
+            nextChildren[parents[n]] = q;
+        }
+        else
+        {
+            previousChildren[parents[n]] = q;
+        }
+
+        parents[n] = nil;
     }
 }
